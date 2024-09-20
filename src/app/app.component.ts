64485dbcd7c42bd865d9f01dda33c9f723a01aa0
Aryan Keyhani <w0849138@myscc.ca>
2024-09-20 14:32:14 -0400

    Assignment 2 finished.Instances created, app.component.html was created and css was added.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { User } from './User';
+import { NgForOf, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgForOf, NgIf],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  name: string = 'Aryan Keyhani';
-  age: number = 19;
-  title = 'Aryan-Learning-Angular';
+  title = 'Aryan Angular' ;
+
+  userList: User[] = [
+    { id: 1, firstName: "Aryan", lastName: "Keyhani", department: "Programming", isAdmin: true },
+    { id: 2, firstName: "Yuanhao", lastName: "Chan", department: "Programming", isAdmin: true },
+    { id: 3, firstName: "John", lastName: "Doe", department: "Programming", isAdmin: false },
+    { id: 4, firstName: "Jane", lastName: "Moe", department: "Programming", isAdmin: false },
+    { id: 5, firstName: "Alice", lastName: "Smith", department: "Design", isAdmin: false },
+    { id: 6, firstName: "Bob", lastName: "Johnson", department: "HR", isAdmin: false }
+  ];
 }
