7ed5e5316abd413fddb0528f92d282b7a27fdba9
Aryan Keyhani <w0849138@myscc.ca>
2024-09-28 02:42:51 -0400

    Steps 5,6,7,8 were completed, arrays added.

--- src/app/game-list-item/game-list-item.component.ts
+++ src/app/game-list-item/game-list-item.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-game-list-item',
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './game-list-item.component.css'
 })
 export class GameListItemComponent {
-
+  @Input() game!: { title: string; genre: string; developer: string; releaseDate: string; rating: string };
 }
--- src/app/game-list/game-list.component.ts
+++ src/app/game-list/game-list.component.ts
@@ -7,6 +7,11 @@ import { Component } from '@angular/core';
   templateUrl: './game-list.component.html',
   styleUrl: './game-list.component.css'
 })
-export class GameListComponent {
-
+export class ContentListComponent {
+  gameList = [
+    {title: 'The Legend of Zelda: Breath of the Wild', genre: 'Action-adventure', developer: 'Nintendo', releaseDate: 'March 3, 2017', rating: '10/10'},
+    {title: 'Cyberpunk 2077', genre: 'RPG', developer: 'CD Projekt Red', releaseDate: 'December 10, 2020', rating: '7/10'},
+    {title: 'God of War', genre: 'Action-adventure', developer: 'Santa Monica Studio', releaseDate: 'April 20, 2018', rating: '9.5/10'},
+    {title: 'Minecraft', genre: 'Sandbox, Survival', developer: 'Mojang Studios', releaseDate: 'November 18, 2011', rating: '9/10'}
+  ];
 }
