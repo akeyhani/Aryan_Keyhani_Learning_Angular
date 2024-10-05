7449dae462ceb5979fc9107f63c208c023ae5df2
Aryan Keyhani <w0849138@myscc.ca>
2024-10-05 02:55:45 -0400

    step 6,7 completed.

--- src/app/game-list/game-list.component.ts
+++ src/app/game-list/game-list.component.ts
@@ -1,27 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-import { NgClass, NgForOf } from "@angular/common";
-import { Games } from '../Games';
+import { NgClass, NgForOf, CommonModule } from "@angular/common";
+import { Games } from '../Games';  // Import the Games model
 import { GameService } from '../game.service';  // Import the GameService
 import { GameListItemComponent } from '../game-list-item/game-list-item.component';
+import { Observable } from 'rxjs';  // Import Observable
 
 @Component({
   selector: 'app-game-list',
   standalone: true,
-  imports: [NgClass, NgForOf, GameListItemComponent],
+  imports: [NgClass, NgForOf, GameListItemComponent, CommonModule, NgForOf],
   templateUrl: './game-list.component.html',
   styleUrls: ['./game-list.component.css']
 })
-
 export class GameListComponent implements OnInit {
-  // Store the games in an array
-  gameList: Games[] = [];
+  // Store the games in an observable array
+  gameList$!: Observable<Games[]>;
 
   // Inject the GameService in the constructor
   constructor(private gameService: GameService) {}
 
   // Use ngOnInit to fetch the games when the component initializes
   ngOnInit(): void {
-    this.gameList = this.gameService.getAllGames();
+    this.gameList$ = this.gameService.getAllGames();  // Fetch the games as an observable
   }
 
   // Example method to add a new game (optional)
@@ -34,6 +34,6 @@ export class GameListComponent implements OnInit {
       rating: '8/10'
     };
     this.gameService.addGame(newGame);
-    this.gameList = this.gameService.getAllGames();  // Refresh the game list
+    this.gameList$ = this.gameService.getAllGames();  // Refresh the game list
   }
 }
